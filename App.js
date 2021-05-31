9b16418e1c496fe77428bca71338cbe70e75f19e
RashidRashie <rashidrashie.a@gmail.com>
2021-05-31 20:38:24 +0530

    Backend user Creation Completed

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('./src/config/db');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
 
@@ -13,20 +15,26 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 //*********Set Static Files */
-//static Files
 app.use(express.static('public'));
 app.use('/css',express.static(__dirname+'public/css'));
 app.use('/img',express.static(__dirname+'public/img'));
+
+//************set Flash Messages */
+app.use(session({
+    secret: 'secret',
+    cookie: {maxAge:60000},
+    resave: false,
+    saveUninitialized:false
+}));
+app.use(flash());
 
 //*******Set template Engine */
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 //********Set Routes */
-
 const userRouter =  require('./src/routes/userRouter');
-
-app.use("/",userRouter);
+app.use("",userRouter);
 
 
 //******Port Listening */
